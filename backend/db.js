d369d74af6c11bb8d1daa44a3e49cfc9d1eae684
Wahyu Jebredz <wabredz1234@gmail.com>
2024-02-22 10:56:03 +0700

    second commit

---
+++ backend/db.js
@@ -0,0 +1,9 @@
+import mariadb from "mariadb";
+
+// export const connection = await mariadb.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "1234",
+//   database: "presensi_shalat",
+// });
